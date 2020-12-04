fd3e621543b43ea7e52c626b0478cd666637e5fe
Jennifer Presto <jennifer@potiondesign.com>
2020-12-04 14:01:50 -0600

    added second part of day 4

--- Day04.cs
+++ Day04.cs
@@ -16,22 +16,8 @@ class Day04
         foreach (Dictionary<string, string> passport in passportList)
         {
             numValid += isPassportValid(passport) ? 1 : 0;
-            if (isPassportValid(passport))
-            {
-                Console.WriteLine("VALID");
-                if (passport.Count == 7)
-                {
-                    Console.WriteLine("Elf passport!!");
-                }
-            }
-            else
-            {
-                Console.WriteLine("INVALID");
-            }
-            printPassport(passport);
         }
         Console.WriteLine($"Valid passports: {numValid}");
-
     }
 
     private List<Dictionary<string, string>> parseData()
@@ -111,27 +97,49 @@ class Day04
         return passportObj;
     }
 
-    private bool isPassportValid(Dictionary<string, string> passportObj)
+    private bool isPassportValid(Dictionary<string, string> passport)
     {
-        if (passportObj.Count == 8)
-        {
-            return true;
-        }
+        //  validity for part one
+        bool partOneValid = (passport.Count == 8) || (passport.Count == 7 && !passport.ContainsKey("cid"));
+        if (!partOneValid) return false;
 
-        if (passportObj.Count == 7 && !passportObj.ContainsKey("cid"))
+        //  validity for part two
+        if (int.Parse(passport["byr"]) < 1920 || int.Parse(passport["byr"]) > 2002) return false; // birth year
+        if (int.Parse(passport["iyr"]) < 2010 || int.Parse(passport["iyr"]) > 2020) return false; // issue year
+        if (int.Parse(passport["eyr"]) < 2020 || int.Parse(passport["eyr"]) > 2030) return false; // issue year
+        if (!passport["hgt"].EndsWith("in") && !passport["hgt"].EndsWith("cm")) return false; // height format
+        string height = passport["hgt"].Substring(0, passport["hgt"].Length - 2); // height value
+        if (passport["hgt"].EndsWith("cm"))
         {
-            return true;
+            if (int.Parse(height) < 150 || int.Parse(height) > 193) return false;
         }
-        return false;
+        else
+        {
+            if (int.Parse(height) < 59 || int.Parse(height) > 76) return false;
+        }
+        if (!passport["hcl"].StartsWith("#")) return false; // haircolor hex start
+        if (!System.Text.RegularExpressions.Regex.IsMatch(passport["hcl"].Substring(1), @"\A\b[0-9a-fA-F]+\b\Z")) return false; // hex value
+        if (passport["ecl"] != "amb" &&
+            passport["ecl"] != "blu" &&
+            passport["ecl"] != "brn" &&
+            passport["ecl"] != "gry" &&
+            passport["ecl"] != "grn" &&
+            passport["ecl"] != "hzl" &&
+            passport["ecl"] != "oth"
+        ) return false; // eye color
+        if (!System.Text.RegularExpressions.Regex.IsMatch(passport["pid"], @"^\d{9}$")) return false;
+        // Console.WriteLine(passport["ecl"]);
+
+        return true;
     }
 
     //  for debugging
     private void printPassport(Dictionary<string, string> passportObj)
     {
-        Console.WriteLine("***********Passport");
+        Console.WriteLine("***********Passport**********");
         foreach (KeyValuePair<string, string> kvp in passportObj)
         {
-            Console.WriteLine($"\tKey: {kvp.Key}, Value: {kvp.Value}");
+            Console.WriteLine($"\t{kvp.Key}: {kvp.Value}");
         }
     }
 }

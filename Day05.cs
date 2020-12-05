5d77511d4e1a21fe7c27b30e10af540939dd1680
Jennifer Presto <jennifer@potiondesign.com>
2020-12-05 11:52:28 -0600

    fixed stupid typo

--- Day05.cs
+++ Day05.cs
@@ -11,26 +11,29 @@ class Day05
 
     public void calculate()
     {
-        for (int i = 0; i < 2; i++)
+        int highestSeat = 0;
+        for (int i = 0; i < data.Count; i++)
         {
-            parsePass(data[i]);
+            int seat = parsePass(data[i]);
+            Console.WriteLine($"pass {i}: {data[i]}: {seat}");
+            if (seat > highestSeat)
+            {
+                highestSeat = seat;
+            }
         }
-        // foreach (string line in data)
-        // {
-        //     parsePass(line);
-        // }
+        Console.WriteLine($"Highest seat: {highestSeat}");
     }
 
     private int parsePass(string pass)
     {
-        Console.WriteLine($"Full boarding pass: {pass}");
-        int seat = 0;
+        // Console.WriteLine($"Full boarding pass: {pass}");
         string rowStr = pass.Substring(0, 7);
         string colStr = pass.Substring(7);
         int row = parseRow(rowStr);
         int col = parseColumn(colStr);
-        Console.WriteLine(pass);
 
+        int seat = row * 8 + col;
+        // Console.WriteLine($"Seat: {seat}");
         return seat;
     }
 
@@ -41,19 +44,24 @@ class Day05
 
     private int parseColumn(string passCol)
     {
-        return parseUnit(passCol, 'L', 'R', 0, 8);
+        return parseUnit(passCol, 'L', 'R', 0, 7);
+    }
+
+    private int parseBinary(string unit, string zero, string one)
+    {
+        return 0;
     }
 
     private int parseUnit(string unit, char lowerChar, char upperChar, int lower, int upper)
     {
-        Console.WriteLine($"{unit} -- lower: {lower}, upper: {upper}");
+        // Console.WriteLine($"{unit} -- lower: {lower}, upper: {upper}");
 
         //  Final stage
         if (unit.Length == 0)
         {
             if (upper != lower)
             {
-                Console.WriteLine("Problem with parsing! Indices don't match!");
+                Console.WriteLine($"Problem with parsing! Indices don't match! {lower} {upper}");
             }
             // string type = lowerChar == 'F' ? "row" : "col";
             // Console.WriteLine($"We're done! {type} {lower} {upper}");
